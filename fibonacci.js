5a98967b7f085c6a66ce8ac3e0896bb5259eca74
Pragati-Verma <30720974+Pragati-Verma@users.noreply.github.com>
2023-08-19 23:27:15 +0530

    Create fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,24 @@
+function fibonacciGenerator (n) {
+
+    var arr = [];
+    if(n === 0 || n === 1)
+        return [0];
+        
+    var num1 = 0;
+    var num2 = 1;
+    
+    while (n > 0){
+        arr.push(num1);
+        var sum = num1 + num2;
+        //arr.push(sum);
+        num2 = num1;
+        num1 = sum;
+        n--;
+    }
+    
+    return arr;
+    
+}
+
+var result = fibonacciGenerator(2);
+console.log(result);
